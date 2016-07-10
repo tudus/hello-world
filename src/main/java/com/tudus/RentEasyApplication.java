839bd0a3fcb9b6a7a0f5e7e2fa73d2f98b7d3c98
tudus <d.ganesh02@gmail.com>
2016-07-10 16:35:06 +0530

    Second commit from Git Extensions

---
+++ src/main/java/com/tudus/RentEasyApplication.java
@@ -0,0 +1,12 @@
+package com.tudus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentEasyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tudus/RentEasyApplicationTests.java
@@ -0,0 +1,18 @@
+package com.tudus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = RentEasyApplication.class)
+@WebAppConfiguration
+public class RentEasyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
